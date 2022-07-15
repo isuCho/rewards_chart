587f99a4fba471bb23a7501e46037278fdac3198
isuCho <chris.eesoo.cho@gmail.com>
2022-07-16 10:00:02 +1200

    Created new child model and added to admin page

--- parents/models.py
+++ parents/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Child(models.Model):
+    name = models.TextField()
+    points = models.BigIntegerField(default=0)
+
+    def __str__(self):
+        return f'({self.name[:30]}: {self.points} points)'
--- parents/tests.py
+++ parents/tests.py
@@ -8,3 +8,6 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(response.status_code, 200)
+
+    # def test_table_adds_
